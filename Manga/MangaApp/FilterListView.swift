ff31ace9e6a089256140637697271eb691579e41
franmu94 <fmalourarte@gmail.com>
2024-06-11 09:26:39 +0200

    filtros 2

---
+++ Manga/MangaApp/FilterListView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterListView.swift
+//  MangaApp
+//
+//  Created by Fran Malo on 10/6/24.
+//
+
+import SwiftUI
+
+struct FilterListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterListView()
+}
---
+++ Manga/MangaApp/FilterListViewArray.swift
@@ -0,0 +1,18 @@
+//
+//  FilterListViewArray.swift
+//  MangaApp
+//
+//  Created by Fran Malo on 10/6/24.
+//
+
+import SwiftUI
+
+struct FilterListViewArray: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterListViewArray()
+}
