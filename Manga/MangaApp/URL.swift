ad49944bfbd8ea380f814deeb81b6e37f5edb8ed
franmu94 <fmalourarte@gmail.com>
2024-06-28 17:52:34 +0200

    url actualizado4

--- Manga/MangaApp/URL.swift
+++ Manga/MangaApp/URL.swift
@@ -11,11 +11,14 @@ let mainURL = URL(string: "https://mymanga-acacademy-5607149ebe3d.herokuapp.com"
 
 extension URL {
     static let userRegisterURL = mainURL.appending(path: "users")
+    static let userRenewURL = userRegisterURL.appending(path: "renew")
     static let userLoginURL = userRegisterURL.appending(path: "login")
 
     private static let listMangasURL = mainURL.appending(path: "list")
     private static let searchMangaContainsURL = mainURL.appending(path: "search/mangasContains")
     
+    static let userCollectionURL = mainURL.appending(path: "collection/manga")
+
 
     static func allMagasList(page: Int, per: Int) -> URL {
         listMangasURL.appending(path: "mangas").appending(queryItems: [.page(page: page), .per(per: per)])
